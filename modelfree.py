59d240c2098b6c2359d9dfbcb1be4c7eca062c19
Joshua Lee <LeeJoshuaK@gmail.com>
2013-04-22 23:56:38 -0400

    started Q learning

--- modelfree.py
+++ modelfree.py
@@ -1,6 +1,7 @@
 from random import *
 import throw
 import darts
+import math
  
 # The default player aims for the maximum score, unless the
 # current score is less than the number of wedges, in which
@@ -33,4 +34,31 @@ def ex_strategy_two():
 
 # The Q-learning algorithm:
 def Q_learning():
-  return
+  gamma = 0.5
+  alpha = 0.5
+  
+  states = darts.get_states()
+  actions = darts.get_actions()
+  
+  Q = {}
+  
+  for s in states:
+    Q[s] = {}
+    for a in actions:
+      Q[s][a] = 0.0
+  
+  converged = False
+  while not converged:
+    
+    action = ex_strategy_one()
+    # action = ex_strategy_two()
+    
+    max_Qsa = float("-inf")
+    for a in actions:
+      score = throw.location_to_score(a)
+      new_max = Q[s - score][a] # s' = s - score
+      if new_max > max_Qsa:
+        max_Qsa = new_max
+    
+    reward = darts.R(s, action)
+    Q[s][action] = Q[s][action] + alpha * (reward + gamma * max_Qsa - Q[s][a])
